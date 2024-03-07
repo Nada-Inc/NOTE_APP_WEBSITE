66943cdb71833fbc48739ad31e9ec5b9054f929c
ananduremanan <ananduor@gmail.com>
2024-03-07 12:32:51 +0530

    Updated Page

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABSE_URL;
-const supabasekey = import.meta.env.VITE_SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabasekey);
