f4fcaae1f23092e55ffebbbe1d530146a5eaeb43
ananduremanan <ananduor@gmail.com>
2024-03-12 14:45:03 +0530

    OsInfo Update

--- src/lib/getOsInfo.ts
+++ src/lib/getOsInfo.ts
@@ -1,14 +1,15 @@
 export const getOSInfo = () => {
 	const userAgent = navigator.userAgent;
+
 	if (userAgent.indexOf('Win') !== -1) {
 		return 'Windows';
 	} else if (userAgent.indexOf('Mac') !== -1) {
 		return 'macOS/iOS';
 	} else if (userAgent.indexOf('Linux') !== -1) {
-		return 'Linux';
+		return 'Android';
 	} else if (userAgent.indexOf('Android') !== -1) {
 		return 'Android';
-	} else if (userAgent.indexOf('iOS') !== -1) {
+	} else if (userAgent.indexOf('iPhone') !== -1) {
 		return 'iOS';
 	}
 };
